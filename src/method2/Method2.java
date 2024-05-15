793cd7e9ef51bfa7953ff1ec01251ce3df43b516
soumyaathanimath <>
2024-05-15 15:07:42 +0530

    string method2

---
+++ src/method2/Method2.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package method2;
+
+/**
+ *
+ * @author dell1
+ */
+public class Method2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+          /*
+        String str1="Mr. Shahrukh Khan";
+        System.out.println(str1.startsWith("Mr."));
+        System.out.println(str1.startsWith("Shah" , 4));
+        System.out.println(str1.endsWith("Khan"));
+        System.out.println(str1.charAt(4));
+        
+        for(int i=0;i<str1.length();i++)
+            System.out.print(str1.charAt(i));
+        */
+        
+        String str1="www.udemy.co.in";
+        System.out.println(str1.indexOf("."));
+        System.out.println(str1.indexOf(".",4));
+        System.out.println(str1.indexOf("udemy"));
+        System.out.println(str1.lastIndexOf("."));
+        
+    }
+    
+}
